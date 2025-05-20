1a8df7d008ec21068fc7806528f6c110954094b8
Sean Kwak <sean.kwak@iohk.io>
2025-05-20 10:37:17 +0100

    feat: unshielded token implementation (ut query and transaction query and ut subscription)

--- indexer-api/src/main.rs
+++ indexer-api/src/main.rs
@@ -82,7 +82,8 @@ async fn run() -> anyhow::Result<()> {
             .context("run Postgres migrations")?;
     }
     let cipher = make_cipher(secret).context("make cipher")?;
-    let storage = infra::storage::postgres::PostgresStorage::new(cipher, pool);
+    let storage =
+        infra::storage::postgres::PostgresStorage::new(cipher, pool, api_config.network_id);
 
     let ledger_state_storage =
         ledger_state_storage::nats::NatsLedgerStateStorage::new(ledger_state_storage_config)
