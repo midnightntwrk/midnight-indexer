48fe42f8d5b710bc045f6262c900ad51cccceea7
Heiko Seeberger <hseeberger@users.noreply.github.com>
2025-08-08 18:50:38 +0200

    test: improve node example (#256)

--- chain-indexer/examples/node.rs
+++ chain-indexer/examples/node.rs
@@ -1,34 +1,65 @@
 use anyhow::Context;
 use chain_indexer::{
-    domain::Node,
+    domain::{Block, Node},
     infra::subxt_node::{Config, SubxtNode},
 };
-use futures::{StreamExt, TryStreamExt};
+use clap::Parser;
+use futures::{Stream, StreamExt, TryStreamExt};
 use indexer_common::domain::{NetworkId, PROTOCOL_VERSION_000_013_000};
-use std::{pin::pin, time::Duration};
+use std::{pin::Pin, time::Duration};
 
-/// This program connects to a local node and prints some first blocks and their transactions.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let config = Config {
-        url: "ws://localhost:9944".to_string(),
-        genesis_protocol_version: PROTOCOL_VERSION_000_013_000,
-        reconnect_max_delay: Duration::from_secs(1),
-        reconnect_max_attempts: 3,
-    };
-    let mut node = SubxtNode::new(config).await.context("create SubxtNode")?;
+    Cli::parse().run().await
+}
 
-    let blocks = node.finalized_blocks(None, NetworkId::Undeployed).take(60);
-    let mut blocks = pin!(blocks);
-    while let Some(block) = blocks.try_next().await.context("get next block")? {
-        println!("## BLOCK: height={}, \thash={}", block.height, block.hash);
-        for transaction in block.transactions {
-            println!(
-                "    ## TRANSACTION: hash={}, \t{transaction:?}",
-                transaction.hash
-            );
+/// This program connects to a node and prints blocks and their transactions.
+#[derive(Debug, Parser)]
+#[command()]
+struct Cli {
+    /// The node URL; defaults to "ws://localhost:9944".
+    #[arg(long, default_value = "ws://localhost:9944")]
+    node: String,
+
+    /// How many blocks to skip; none if omitted.
+    #[arg(long)]
+    skip: Option<usize>,
+
+    /// How many blocks to take; unlimited if omitted.
+    #[arg(long)]
+    take: Option<usize>,
+}
+
+impl Cli {
+    async fn run(self) -> anyhow::Result<()> {
+        let config = Config {
+            url: self.node,
+            genesis_protocol_version: PROTOCOL_VERSION_000_013_000,
+            reconnect_max_delay: Duration::from_secs(1),
+            reconnect_max_attempts: 1,
+        };
+        let mut node = SubxtNode::new(config).await.context("create SubxtNode")?;
+
+        let blocks = node.finalized_blocks(None, NetworkId::Undeployed);
+        let mut blocks: Pin<Box<dyn Stream<Item = Result<Block, _>> + Send>> = Box::pin(blocks);
+
+        if let Some(n) = self.skip {
+            blocks = Box::pin(blocks.skip(n));
         }
-    }
 
-    Ok(())
+        if let Some(n) = self.take {
+            blocks = Box::pin(blocks.take(n));
+        }
+
+        while let Some(block) = blocks.try_next().await.context("get next block")? {
+            println!("## BLOCK: height={}, \thash={}", block.height, block.hash);
+            for transaction in block.transactions {
+                println!(
+                    "    ## TRANSACTION: hash={}, \t{transaction:?}",
+                    transaction.hash
+                );
+            }
+        }
+        Ok(())
+    }
 }
