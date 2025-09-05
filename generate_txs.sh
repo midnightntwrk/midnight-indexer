7fe0c2fdc4605b8bbc822f9fc2b240a7a6c9ef33
Heiko Seeberger <git@heikoseeberger.de>
2025-09-05 11:55:12 +0200

    fix: correctly determine transaction relevance

--- generate_txs.sh
+++ generate_txs.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
+if [ -z "$1" ]; then
+    echo "Error: node version parameter is required" >&2
+    echo "Usage: $0 <node_version>" >&2
+    exit 1
+fi
+node_version="$1"
+
 # 1 to 2/2
-midnight-node-toolkit generate-txs \
-    --dest-file ./target/tx_1_2_2.mn \
+docker run \
+    --rm \
+    --network host \
+    -v ./target:/out \
+    ghcr.io/midnight-ntwrk/midnight-node-toolkit:$node_version \
+    generate-txs \
+    --dest-file /out/tx_1_2_2.mn \
     --to-bytes \
     single-tx \
     --shielded-amount 10 \
@@ -11,14 +25,24 @@ midnight-node-toolkit generate-txs \
     --destination-address mn_shield-addr_undeployed1tffkxdesnqz86wvds2aprwuprpvzvag5t3mkveddr33hr7xyhlhqxqzfqqxy54an7cyznaxnzs7p8tduku7fuje5mwqx9auvdn9e8x03kvvy5r6z \
     --destination-address mn_addr_undeployed1gkasr3z3vwyscy2jpp53nzr37v7n4r3lsfgj6v5g584dakjzt0xqun4d4r
 
-midnight-node-toolkit get-tx-from-context \
+docker run \
+    --rm \
+    --network host \
+    -v ./target:/out \
+    ghcr.io/midnight-ntwrk/midnight-node-toolkit:$node_version \
+    get-tx-from-context \
     --src-file ./target/tx_1_2_2.mn \
     --network undeployed \
     --dest-file ./target/tx_1_2_2.raw \
     --from-bytes
 
 # 1 to 2/3
-midnight-node-toolkit generate-txs \
+docker run \
+    --rm \
+    --network host \
+    -v ./target:/out \
+    ghcr.io/midnight-ntwrk/midnight-node-toolkit:$node_version \
+    generate-txs \
     --dest-file ./target/tx_1_2_3.mn \
     --to-bytes \
     single-tx \
@@ -28,7 +52,12 @@ midnight-node-toolkit generate-txs \
     --destination-address mn_shield-addr_undeployed1tffkxdesnqz86wvds2aprwuprpvzvag5t3mkveddr33hr7xyhlhqxqzfqqxy54an7cyznaxnzs7p8tduku7fuje5mwqx9auvdn9e8x03kvvy5r6z \
     --destination-address mn_addr_undeployed1g9nr3mvjcey7ca8shcs5d4yjndcnmczf90rhv4nju7qqqlfg4ygs0t4ngm
 
-midnight-node-toolkit get-tx-from-context \
+docker run \
+    --rm \
+    --network host \
+    -v ./target:/out \
+    ghcr.io/midnight-ntwrk/midnight-node-toolkit:$node_version \
+    get-tx-from-context \
     --src-file ./target/tx_1_2_3.mn \
     --network undeployed \
     --dest-file ./target/tx_1_2_3.raw \
