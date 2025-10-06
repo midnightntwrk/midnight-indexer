507c2db30c637ba38db0b6678bc419f375a078b3
Sean Kwak <sean.kwak@iohk.io>
2025-10-06 15:04:12 +0100

    fmt

--- indexer-tests/tests/native_e2e.rs
+++ indexer-tests/tests/native_e2e.rs
@@ -313,8 +313,9 @@ async fn start_indexer_api(postgres_port: u16, nats_url: &str) -> anyhow::Result
             format!("{}/indexer-api/config.yaml", &*WS_DIR),
         )
         .env("APP__INFRA__API__PORT", api_port.to_string())
-        // BlockSubscription query unchanged from main but test fails with query complexity exceeded.
-        // Not investigating deeply since we're not merging everything from feat/cnight-generates-dust to main.
+        // BlockSubscription query unchanged from main but test fails with query complexity
+        // exceeded. Not investigating deeply since we're not merging everything from
+        // feat/cnight-generates-dust to main.
         .env("APP__INFRA__API__MAX_COMPLEXITY", "600")
         .env("APP__INFRA__PUB_SUB__URL", nats_url)
         .env("APP__INFRA__STORAGE__PORT", postgres_port.to_string())
@@ -341,7 +342,10 @@ fn start_indexer_standalone(node_url: &str) -> anyhow::Result<(Child, u16, TempD
             format!("{}/indexer-standalone/config.yaml", &*WS_DIR),
         )
         .env("APP__INFRA__API__PORT", api_port.to_string())
-        .env("APP__INFRA__API__MAX_COMPLEXITY", "500")
+        // BlockSubscription query unchanged from main but test fails with query complexity
+        // exceeded. Not investigating deeply since we're not merging everything from
+        // feat/cnight-generates-dust to main.
+        .env("APP__INFRA__API__MAX_COMPLEXITY", "600")
         .env("APP__INFRA__NODE__URL", node_url)
         .env("APP__INFRA__STORAGE__CNN_URL", sqlite_file)
         .env("APP__TELEMETRY__TRACING__ENABLED", "true")
