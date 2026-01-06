689d86c136605d56edaa03116cc12890bd46561b
Chris Ferry <chris.ferry@shielded.io>
2026-01-06 11:37:40 -0500

    Remove changelog update and move index creation to 001

--- indexer-common/migrations/postgres/003_add_transaction_variant_index.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------------------------------
--- transactions variant index
---------------------------------------------------------------------------------
--- Add composite index on (variant, id) to optimize wallet-indexer queries
--- that filter on variant and scan from a starting transaction id.
--- Query pattern: WHERE id >= ? AND variant = 'Regular' ORDER BY id LIMIT ?
--- See: https://github.com/midnightntwrk/midnight-indexer/issues/646
-CREATE INDEX ON transactions (variant, id);
