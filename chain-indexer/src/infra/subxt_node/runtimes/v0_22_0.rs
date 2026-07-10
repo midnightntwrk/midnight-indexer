3b9bf1f8e94da1ac8ec63eb1ade517e901feda9b
Sean Kwak <sean.kwak@iohk.io>
2026-07-10 15:14:30 +0100

    Update chain-indexer/src/infra/subxt_node/runtimes/v0_22_0.rs
    Co-authored-by: Squirrel <giles.cope@shielded.io>

--- chain-indexer/src/infra/subxt_node/runtimes/v0_22_0.rs
+++ chain-indexer/src/infra/subxt_node/runtimes/v0_22_0.rs
@@ -166,7 +166,7 @@ pub async fn make_block_details(
         timestamp,
         transactions,
         dust_registration_events,
-        bridge_pallet_events: vec![],
+        bridge_events: vec![],
     })
 }
 
